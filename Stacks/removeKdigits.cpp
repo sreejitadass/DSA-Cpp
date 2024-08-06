8f64d280752e48544fb204e5c16e91703675c0d1
Sreejita Das <sreejitadas@Sreejitas-Laptop.local>
2024-08-07 00:34:22 +0530

    new codes added

---
+++ Stacks/removeKdigits.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+string removeKdigits(string num, int k)
+{
+    stack<char> s;
+    //push and pop till k>0
+    for (int i = 0; i < num.size(); i++)
+    {
+        while (!s.empty() && k > 0 && s.top()-'0' > num[i]-'0')
+        {
+            s.pop();
+            k--;
+        }
+        s.push(num[i]);
+    }
+    //for 123456 test cases
+    while (k > 0 && !s.empty())
+    {
+        s.pop();
+        k--;
+    }
+
+    //if all r removed - k==n
+    if (s.empty())
+        return "0";
+
+    string ans = "";
+    while (!s.empty())
+    {
+        ans += s.top();
+        s.pop();
+    }
+    reverse(ans.begin(), ans.end());
+
+    // Remove leading zeros + all zeroes case
+    while (!ans.empty() && ans[0] == '0')
+    {
+        ans.erase(ans.begin());
+    }
+    return ans.empty() ? "0" : ans;
+}
+
+int main()
+{
+    string num = "100";
+    int k = 1;
+    cout << "Smallest number: " << removeKdigits(num, k) << endl;
+    return 0;
+}
---
+++ Stacks/stockSpan.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<utility>
+using namespace std;
+
+/*Brute-Force*/
+// class StockSpanner {
+//     vector<int>arr;
+// public:
+//     StockSpanner() {
+//         arr={};
+//     }
+    
+//     int next(int price) {
+//        arr.push_back(price);
+//        int count=0;
+//        for(int i = arr.size()-1;i>=0;i--)
+//        {
+//           if(arr[i]<=price)
+//             count++;
+//        }
+//        return count;
+//     }
+// };
+
+/*Optimized*/
+class StockSpanner {
+    stack<pair<int,int>>s;
+    int index;
+public:
+    StockSpanner() {
+        s = {};
+        index = -1;
+    }
+    
+    int next(int price) {
+       index++;
+       while(!s.empty() && s.top().first <= price)
+       {
+            s.pop();
+       }
+       int ans = (index-(s.empty() ? -1: s.top().second));
+       s.push({price,index});
+       return ans;
+    }
+};
+
+int main()
+{
+    StockSpanner stocks;
+    cout<<stocks.next(100)<<endl;
+    cout<<stocks.next(80)<<endl;
+    cout<<stocks.next(60)<<endl;
+    cout<<stocks.next(70)<<endl;
+    cout<<stocks.next(60)<<endl;
+    cout<<stocks.next(75)<<endl;
+    cout<<stocks.next(85)<<endl;
+    return 0;
+}
